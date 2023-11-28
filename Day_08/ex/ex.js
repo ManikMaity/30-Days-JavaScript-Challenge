ca03af4a57157a6791ee5a842ef8be97efbb70a9
Manik Maity <manikmaity156@gmail.com>
2023-11-28 21:18:42 +0530

    Completed level2 and started level2 ex of Day_08

--- Day_08/ex/ex.js
+++ Day_08/ex/ex.js
@@ -127,3 +127,139 @@ console.log(usersAllValues);
 // Use the countries object to print a country name, capital, populations and languages. 
 var countryData = require('./countries.js')
 console.log(countryData.COUNTRIES[0]);
+
+
+//  -------------------------------------- LEVEL 3 ------------------------------------------
+
+
+// Create an object literal called personAccount. It has firstName, lastName, incomes, expenses 
+// properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. 
+// Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+
+let personAccount = {
+  firstName : "Manik",
+  lastName : "Maity",
+  incomes : {salary: 8000, formMama:4000},
+  exepenses: {recharge: 400, mobileRepair: 600},
+  totalIncome  : function() {
+    const allIncomes = Object.values(this.incomes);
+    let sum = 0;
+    for (let i = 0; i < allIncomes.length; i++){
+      sum += allIncomes[i];
+    }
+    return sum;
+  },
+
+  totalExpences : function () {
+    const allExpences  = Object.values(this.exepenses);
+    let sum = 0;
+    for (let i = 0; i < allExpences.length; i++){
+      sum += allExpences[i];
+    }
+    return sum;
+  },
+
+  addIncome : function (key = "Key1", value = 200) {
+    this.incomes[key] = value;
+  },
+
+  addExpences : function (key = "Key1", value = 200) {
+    this.exepenses[key] = value;
+  },
+
+  accountBalance : function () {
+    let income = this.totalIncome();
+    let expence = this.totalExpences();
+
+    return `Account Balance = (${income} - ${expence}) = ${income - expence}`;
+  }
+};
+
+console.log(personAccount.totalIncome()) //12000
+console.log(personAccount.totalExpences()) //1000
+
+console.log(personAccount.addIncome("fromNotes", 100));
+console.log(personAccount.incomes); // { salary: 8000, formMama: 4000, fromNotes: 100 }
+console.log(personAccount.accountBalance()); // Account Balance = (12100 - 1000) = 11100
+
+
+// **** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
+const users2 = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: false
+  }
+  ];
+
+  const products = [
+{
+  _id: 'eedfcf',
+  name: 'mobile phone',
+  description: 'Huawei Honor',
+  price: 200,
+  ratings: [
+    { userId: 'fg12cy', rate: 5 },
+    { userId: 'zwf8md', rate: 4.5 }
+  ],
+  likes: []
+},
+{
+  _id: 'aegfal',
+  name: 'Laptop',
+  description: 'MacPro: System Darwin',
+  price: 2500,
+  ratings: [],
+  likes: ['fg12cy']
+},
+{
+  _id: 'hedfcg',
+  name: 'TV',
+  description: 'Smart TV:Procaster',
+  price: 400,
+  ratings: [{ userId: 'fg12cy', rate: 5 }],
+  likes: ['fg12cy']
+}
+];
+
+// Create a function called signUp which allows user to add to the collection. 
+// If user exists, inform the user that he has already an account.
+
+function signUp (obj = {}) {
+  
+}
