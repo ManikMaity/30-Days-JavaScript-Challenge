3e77ac8debf8623222bc1094ef7f5be50053ce70
Manik Maity <manikmaity156@gmail.com>
2023-12-04 15:26:00 +0530

    Day_09 ex level3 q1 and q2 done

--- Day_09/ex/ex.js
+++ Day_09/ex/ex.js
@@ -159,257 +159,7 @@ const products2 = [
     { product: 'tea', price: '' },
   ]
 
-  const countries2 = [
-    "Afghanistan",
-	"Albania",
-	"Algeria",
-	"American Samoa",
-	"Andorra",
-	"Angola",
-	"Anguilla",
-	"Antarctica",
-	"Antigua and Barbuda",
-	"Argentina",
-	"Armenia",
-	"Aruba",
-	"Australia",
-	"Austria",
-	"Azerbaijan",
-	"Bahamas (the)",
-	"Bahrain",
-	"Bangladesh",
-	"Barbados",
-	"Belarus",
-	"Belgium",
-	"Belize",
-	"Benin",
-	"Bermuda",
-	"Bhutan",
-	"Bolivia (Plurinational State of)",
-	"Bonaire, Sint Eustatius and Saba",
-	"Bosnia and Herzegovina",
-	"Botswana",
-	"Bouvet Island",
-	"Brazil",
-	"British Indian Ocean Territory (the)",
-	"Brunei Darussalam",
-	"Bulgaria",
-	"Burkina Faso",
-	"Burundi",
-	"Cabo Verde",
-	"Cambodia",
-	"Cameroon",
-	"Canada",
-	"Cayman Islands (the)",
-	"Central African Republic (the)",
-	"Chad",
-	"Chile",
-	"China",
-	"Christmas Island",
-	"Cocos (Keeling) Islands (the)",
-	"Colombia",
-	"Comoros (the)",
-	"Congo (the Democratic Republic of the)",
-	"Congo (the)",
-	"Cook Islands (the)",
-	"Costa Rica",
-	"Croatia",
-	"Cuba",
-	"Curaçao",
-	"Cyprus",
-	"Czechia",
-	"Côte d'Ivoire",
-	"Denmark",
-	"Djibouti",
-	"Dominica",
-	"Dominican Republic (the)",
-	"Ecuador",
-	"Egypt",
-	"El Salvador",
-	"Equatorial Guinea",
-	"Eritrea",
-	"Estonia",
-	"Eswatini",
-	"Ethiopia",
-	"Falkland Islands (the) [Malvinas]",
-	"Faroe Islands (the)",
-	"Fiji",
-	"Finland",
-	"France",
-	"French Guiana",
-	"French Polynesia",
-	"French Southern Territories (the)",
-	"Gabon",
-	"Gambia (the)",
-	"Georgia",
-	"Germany",
-	"Ghana",
-	"Gibraltar",
-	"Greece",
-	"Greenland",
-	"Grenada",
-	"Guadeloupe",
-	"Guam",
-	"Guatemala",
-	"Guernsey",
-	"Guinea",
-	"Guinea-Bissau",
-	"Guyana",
-	"Haiti",
-	"Heard Island and McDonald Islands",
-	"Holy See (the)",
-	"Honduras",
-	"Hong Kong",
-	"Hungary",
-	"Iceland",
-	"India",
-	"Indonesia",
-	"Iran (Islamic Republic of)",
-	"Iraq",
-	"Ireland",
-	"Isle of Man",
-	"Israel",
-	"Italy",
-	"Jamaica",
-	"Japan",
-	"Jersey",
-	"Jordan",
-	"Kazakhstan",
-	"Kenya",
-	"Kiribati",
-	"Korea (the Democratic People's Republic of)",
-	"Korea (the Republic of)",
-	"Kuwait",
-	"Kyrgyzstan",
-	"Lao People's Democratic Republic (the)",
-	"Latvia",
-	"Lebanon",
-	"Lesotho",
-	"Liberia",
-	"Libya",
-	"Liechtenstein",
-	"Lithuania",
-	"Luxembourg",
-	"Macao",
-	"Madagascar",
-	"Malawi",
-	"Malaysia",
-	"Maldives",
-	"Mali",
-	"Malta",
-	"Marshall Islands (the)",
-	"Martinique",
-	"Mauritania",
-	"Mauritius",
-	"Mayotte",
-	"Mexico",
-	"Micronesia (Federated States of)",
-	"Moldova (the Republic of)",
-	"Monaco",
-	"Mongolia",
-	"Montenegro",
-	"Montserrat",
-	"Morocco",
-	"Mozambique",
-	"Myanmar",
-	"Namibia",
-	"Nauru",
-	"Nepal",
-	"Netherlands (the)",
-	"New Caledonia",
-	"New Zealand",
-	"Nicaragua",
-	"Niger (the)",
-	"Nigeria",
-	"Niue",
-	"Norfolk Island",
-	"Northern Mariana Islands (the)",
-	"Norway",
-	"Oman",
-	"Pakistan",
-	"Palau",
-	"Palestine, State of",
-	"Panama",
-	"Papua New Guinea",
-	"Paraguay",
-	"Peru",
-	"Philippines (the)",
-	"Pitcairn",
-	"Poland",
-	"Portugal",
-	"Puerto Rico",
-	"Qatar",
-	"Republic of North Macedonia",
-	"Romania",
-	"Russian Federation (the)",
-	"Rwanda",
-	"Réunion",
-	"Saint Barthélemy",
-	"Saint Helena, Ascension and Tristan da Cunha",
-	"Saint Kitts and Nevis",
-	"Saint Lucia",
-	"Saint Martin (French part)",
-	"Saint Pierre and Miquelon",
-	"Saint Vincent and the Grenadines",
-	"Samoa",
-	"San Marino",
-	"Sao Tome and Principe",
-	"Saudi Arabia",
-	"Senegal",
-	"Serbia",
-	"Seychelles",
-	"Sierra Leone",
-	"Singapore",
-	"Sint Maarten (Dutch part)",
-	"Slovakia",
-	"Slovenia",
-	"Solomon Islands",
-	"Somalia",
-	"South Africa",
-	"South Georgia and the South Sandwich Islands",
-	"South Sudan",
-	"Spain",
-	"Sri Lanka",
-	"Sudan (the)",
-	"Suriname",
-	"Svalbard and Jan Mayen",
-	"Sweden",
-	"Switzerland",
-	"Syrian Arab Republic",
-	"Taiwan",
-	"Tajikistan",
-	"Tanzania, United Republic of",
-	"Thailand",
-	"Timor-Leste",
-	"Togo",
-	"Tokelau",
-	"Tonga",
-	"Trinidad and Tobago",
-	"Tunisia",
-	"Turkey",
-	"Turkmenistan",
-	"Turks and Caicos Islands (the)",
-	"Tuvalu",
-	"Uganda",
-	"Ukraine",
-	"United Arab Emirates (the)",
-	"United Kingdom of Great Britain and Northern Ireland (the)",
-	"United States Minor Outlying Islands (the)",
-	"United States of America (the)",
-	"Uruguay",
-	"Uzbekistan",
-	"Vanuatu",
-	"Venezuela (Bolivarian Republic of)",
-	"Viet Nam",
-	"Virgin Islands (British)",
-	"Virgin Islands (U.S.)",
-	"Wallis and Futuna",
-	"Western Sahara",
-	"Yemen",
-	"Zambia",
-	"Zimbabwe",
-	"Åland Islands"
-  ]
+  var countries2 = require('./countries2.js');
 
 // Find the total price of products by chaining two or more array iterators
 // (eg. arr.map(callback).filter(callback).reduce(callback))
@@ -515,6 +265,122 @@ var countries3 = require('./countries.js');
 
 // Use the countries information, in the data folder. 
 // Sort countries by name, by capital, by population 
-
+const countries4 = countries3.slice(0);
+function capitalNameSorting (country1 = {}, country2 ={}){
+	if (country1.hasOwnProperty("capital") && country2.hasOwnProperty("capital")){
+		let cap1 = country1.capital.toLowerCase().charAt(0);
+		let cap2 = country2.capital.toLowerCase().charAt(0);
+		if (cap1 > cap2){
+			return 1;
+		}
+		else if (cap1 < cap2){
+			return -1;
+		}
+		else {
+			return 0
+		}
+	}
+	else if (country1.hasOwnProperty("capital")){
+		return -1;
+	}
+	else if (country2.hasOwnProperty("capital")){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+const sortedByCapitalName = countries4.sort(capitalNameSorting)
 console.log("Sorted coutries bases on capitals")
-console.log(capitalSort);
+// console.log(sortedByCapitalName); // --> Working
+
+// Sorting by population 
+const countries5 = countries3.slice(0);
+
+function populationSort (country1, country2){
+	if (country1.hasOwnProperty("population") && country2.hasOwnProperty("population")){
+		if (country1.population > country2.population){
+			return 1;
+		}
+		else if (country1.population < country2.population){
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (country1.hasOwnProperty("population")){
+		return -1;
+	}
+	else if (countries2.hasOwnProperty("population")){
+		return 1
+	}
+	else{
+		return 0;
+	}
+}
+
+// const sortedByPopulation = countries5.sort(populationSort)
+// console.log(sortedByPopulation);// --> working
+
+/* [
+	{
+	  name: 'Bouvet Island',
+	  languages: [ 'Norwegian', 'Norwegian Bokmål', 'Norwegian Nynorsk' ],
+	  population: 0,
+	  flag: 'https://flagcdn.com/bv.svg',
+	  region: 'Antarctic Ocean',
+	  area: 49
+	},
+	{
+	  name: 'Heard Island and McDonald Islands',
+	  languages: [ 'English' ],
+	  population: 0,
+	  flag: 'https://flagcdn.com/hm.svg',
+	  region: 'Antarctic',
+	  area: 412
+	},.....] */
+
+
+
+
+// *** Find the 10 most spoken languages
+const countries6 = countries3.slice(0);
+
+function mostSpokenLanguage (countriesArr, numberOfLang){
+	let mostSpokenLeng = [];
+	
+	// filter out not having language country
+	const filteredLanguageCountry = countriesArr.filter(country => country.hasOwnProperty("languages"));
+	
+	// getting all languages 
+	let allLanguages = [];
+	function pushLang (country){
+		let lang = country.languages;
+		allLanguages.push(lang);
+	}
+	const pushLanguage = filteredLanguageCountry.map(pushLang)
+	allLanguages = allLanguages.reduce((sum, lang) => sum.concat(lang));
+	
+
+	//removing duplicates
+	const indiLanguage = allLanguages.filter((item, index) => allLanguages.indexOf(item) === index);
+
+
+	// language counts and array
+	for (let i = 0; i < indiLanguage.length; i++){
+		let langAndCount = {};
+		let lang = indiLanguage[i];
+		const langCount = allLanguages.filter(item => item == lang).length;
+		langAndCount.country = lang;
+		langAndCount.count = langCount;
+		mostSpokenLeng.push(langAndCount);
+	}
+
+	 // sorting array bassed on language count 
+	 const sortedLangAndCountArray = mostSpokenLeng.sort((a, b) => b.count - a.count);
+
+	 
+	 return sortedLangAndCountArray.slice(0, numberOfLang);
+}
+console.log(mostSpokenLanguage(countries6, 10)) // --> working 
